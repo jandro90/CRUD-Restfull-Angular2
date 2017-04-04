2bdc4ffa441c736a2740207f7b6613086b12f563
Alejandro Gómez <agp901@gmail.com>
2017-04-04 15:43:05 +0200

    Añadiendo spinners, verificación de datos, mensaje de error y success
    -añadido efecto spinners en los botones para dar seguridad al usuario de que sus datos se guardan en firebase correctamente.
    
    - verificación de que los campos esten completos
    
    -mostrando mensaje de error o succes y redirigiendo al usuario para que pueda consultar, editar o eliminar los datos.

--- src/app/componentes/insertar/insertar.component.ts
+++ src/app/componentes/insertar/insertar.component.ts
@@ -17,6 +17,10 @@ export class InsertarComponent implements OnInit {
     key: '',
   };
 
+  private spinner:boolean = false;
+  private msgError:boolean = false;
+  private msgSucces:boolean = false;
+
 
   constructor(private _firebase:FirebaseService, private _activatedRoute:ActivatedRoute, private _router:Router) { }
 
@@ -24,12 +28,39 @@ export class InsertarComponent implements OnInit {
   }
 
   enviarDatos(){
+    //comprobando si el usuario ha completado algo!
 
-    this._firebase.peticionPost(this.datosUsuario).subscribe(data => {
-      this.datosUsuario.key = data.name;
-      //Añadiendo la key al Objeto.
-      this._firebase.peticionPut(this.datosUsuario.key, this.datosUsuario).subscribe();
-    })
+    if(this.datosUsuario.nombre.length >=3 && this.datosUsuario.cargo.length >=3){
+      //Efecto Spiner boton guardar.
+      setTimeout( () =>{
+        this.spinner = true;
+      });
+      setTimeout( () =>{
+        this.spinner = false;
+      },3000);
+
+      //Conectando con el servicio para insertar Registro.
+      this._firebase.peticionPost(this.datosUsuario).subscribe(data => {
+        this.datosUsuario.key = data.name;
+        //Añadiendo la key al Objeto.
+        this._firebase.peticionPut(this.datosUsuario.key, this.datosUsuario).subscribe();
+      });
+
+      this.msgError = false;
+
+      setTimeout( () =>{
+        this.msgSucces = true;
+      },3000);
+
+      setTimeout( () =>{
+        this._router.navigate(['/datos']);
+      },10000);
+    } else{
+      //mostrando mensaje de error si el usuario no completa los campos.
+      this.msgError = true;
+
+    }
+
 
 
 
